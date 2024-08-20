492db0de47f0707c3a41d2715bc7cea0a31bd29d
Manuel Quarneti <manuelquarneti@protonmail.com>
2024-08-20 15:36:22 +0200

    🐛 fix

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -5,57 +5,57 @@
 // hoping it doesn't crash the browser with small numbers
 
 export const getCouples = (peopleCount: number) => {
-  const couples: number[][] = [];
+	const couples: number[][] = [];
 
-  for (let i = 1; i < peopleCount + 1; i++) {
-    for (let j = 1; j < peopleCount + 1; j++) {
-      // no singles please
-      if (i === j) {
-        continue;
-      }
+	for (let i = 1; i < peopleCount + 1; i++) {
+		for (let j = 1; j < peopleCount + 1; j++) {
+			// no singles please
+			if (i === j) {
+				continue;
+			}
 
-      // no duplicates please
-      if (couples.find((couple) => couple[0] === j && couple[1] === i)) {
-        continue;
-      }
+			// no duplicates please
+			if (couples.find((couple) => couple[0] === j && couple[1] === i)) {
+				continue;
+			}
 
-      couples.push([i, j]);
-    }
-  }
+			couples.push([i, j]);
+		}
+	}
 
-  let matrix: number[][][] = [];
+	let matrix: number[][][] = [];
 
-  // fill the matrix with empty arrays
-  // this is a mess
-  for (let i = 0; i < 100; i++) {
-    matrix.push([]);
-  }
+	// fill the matrix with empty arrays
+	// this is a mess
+	for (let i = 0; i < 100; i++) {
+		matrix.push([]);
+	}
 
-  for (const couple of couples) {
-    let present = false;
-  
-    for (const row of matrix) {
-      for (const [a, b] of row) {
-        if (
-          a === couple[0] ||
-          a === couple[1] ||
-          b === couple[0] ||
-          b === couple[1]
-        ) {
-          present = true;
-          break;
-        }
-      }
-  
-      if (!present) {
-        row.push(couple);
-        break;
-      }
-    }
-  }
+	for (const couple of couples) {
+		for (let i = 0; ; i++) {
+			let present = false;
 
-  // remove empty arrays
-  matrix = matrix.filter((array) => array.length > 0);
+			for (const [a, b] of matrix[i]) {
+				if (
+					a === couple[0] ||
+					a === couple[1] ||
+					b === couple[0] ||
+					b === couple[1]
+				) {
+					present = true;
+					break;
+				}
+			}
 
-  return matrix;
+			if (!present) {
+				matrix[i].push(couple);
+				break;
+			}
+		}
+	}
+
+	// remove empty arrays
+	matrix = matrix.filter((array) => array.length > 0);
+
+	return matrix;
 };
